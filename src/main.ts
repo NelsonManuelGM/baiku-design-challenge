074a3a5b11293a585c4473a734107485dff383d6
Nelson <43588033+NelsonManuelGM@users.noreply.github.com>
2021-09-30 13:04:37 -0400

    swagger initial configuration and loggerMiddleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(LoggerMiddleware);
+
+  const config = new DocumentBuilder()
+    .setTitle('Baiku CRUD')
+    .setVersion('1.0')
+    // .addTag('bicycles',)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(5000);
 }
 bootstrap();
