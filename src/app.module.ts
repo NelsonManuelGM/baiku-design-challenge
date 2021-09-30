587254fa70d59479d5e7f55ed36760d22df1ded8
Nelson <43588033+NelsonManuelGM@users.noreply.github.com>
2021-09-30 17:15:18 -0400

    updating docker files and gloval vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { BicyclesModule } from './bicycles/bicycles.module';
+import { Bicycle } from './bicycles/entities/bicycle.entity';
 
 @Module({
-  imports: [BicyclesModule],
+  imports: [
+    BicyclesModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [Bicycle],
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('doc', app, document);
 
-  await app.listen(5000);
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
